cf051aa342ee8638f86bd82435e8111573465e16
miya <752875397@qq.com>
2019-11-27 01:38:23 +0800

    字符串

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-character_name ="miya"
-character_age="25"
-
-print("she name is " + character_name + " and age is " +character_age)
+a='AaBE'
+print("Giraffe \nAcademy")
+print("特殊符号用反斜杠 \ ")
+print(a.lower() + "这是显示小写.lower()")
+print(len(a))
+print(a[0])
+print(a.index("E"))
