5cdf92ca17b1fc06eca64cf650b27f546931fe3c
miyafung <752875397@qq.com>
2019-11-28 14:29:07 +0800

    变量和数据类型

---
+++ Number_type.py
@@ -0,0 +1,31 @@
+print( -2.01002)
+print(3 + 4)
+print(3*4+5)
+
+# 取余数
+print(10%3)
+
+my_num = -199
+print(str(my_num) + " my favorite number")
+# str转换为字符串
+
+#abs 取整数
+print(abs(my_num))
+
+# pow 平方 3的4次方
+print(pow(3,4))
+
+# min取最小数
+print(min(3,1,7))
+
+# round 四舍五入
+print(round(3.2))
+
+# 取整数,去小数点
+from math import *  #引入外部代码，更多不同的数学函数
+print(floor(3.7))
+
+# 有小数区整数+1
+print(ceil(5.4))
+
+print(sqrt(36))
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # pythonst
-st
+
+安装Python和PyCharm
+
+设置和Hello World
+
+图形绘制
+
+变量和数据类型
+
+字符串类型的使用
+
+数字类型的使用
