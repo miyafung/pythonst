fc190bc74b3ef19452623eaeae0b0cf3878d3a33
miyafung <752875397@qq.com>
2019-11-28 10:51:15 +0800

    while循环

--- Variables_dataTypes.py
+++ Variables_dataTypes.py
@@ -2,10 +2,18 @@ secret_word ="abc"
 guess= ""
 guess_count = 0
 guess_limit = 3
-# out_of_guesses = false
+out_of_guesses = False
+# python 计算数组从0 开始
 
 
-while guess != secret_word:
-    guess = input("Enter guess: ")
-    # guess_count +=1
-print("you win!")
+while guess != secret_word and not (out_of_guesses):
+    if guess_count < guess_limit:
+        guess = input("Enter guess: ")
+        guess_count += 1
+    else:
+        out_of_guesses: True
+
+    if out_of_guesses:
+        print("out of guess,you lose")
+    else:
+        print("you win!")
